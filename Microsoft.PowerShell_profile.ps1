bb03afac71af58bc8e9e09cd943c7ac834a2d945
Elmeri Poikolainen <elmeri.poikolainen@eficode.com>
2018-10-29 15:45:47 +0100

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,90 @@
+function vim {
+    $filename = $args[0]
+    bash -c "vi $filename"
+}
+
+function gits {
+    git status
+}
+
+function gitl {
+    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+}
+
+function gitd {
+    git diff
+}
+
+function gitb {
+    git branch
+}
+
+function gitc {
+    $arguments = $args[0]
+    git checkout $arguments
+}
+
+Set-Alias gs gits
+
+Function touch
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
+
+Function gap
+{
+    $message = $args[0]
+    git add .
+    git commit -m "$message"
+    git push
+}
+
+function Write-BranchName () {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            # we're probably in detached HEAD state, so print the SHA
+            $branch = git rev-parse --short HEAD
+            Write-Host " ($branch)" -ForegroundColor "red"
+        }
+        else {
+            # we're on an actual branch, so print it
+            Write-Host " ($branch)" -ForegroundColor "blue"
+        }
+    } catch {
+        # we'll end up here if we're in a newly initiated git repo
+        Write-Host " (no branches yet)" -ForegroundColor "yellow"
+    }
+}
+
+function prompt {
+    $base = "PS "
+    $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+    $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+
+    Write-Host "`n$base" -NoNewline
+
+    if (Test-Path .git) {
+        Write-Host $path -NoNewline -ForegroundColor "green"
+        Write-BranchName
+    }
+    else {
+        # we're not in a repo so don't bother displaying branch name/sha
+        Write-Host $path -ForegroundColor "green"
+    }
+
+    return $userPrompt
+}
